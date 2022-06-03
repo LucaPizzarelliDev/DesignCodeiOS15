7bf8d78b40a5347727bba7ee2643498cdda4f1ed
Luca Pizzarelli <lucapizzarelli@MacBook-Pro-di-Luca.local>
2022-06-03 10:08:29 +0200

    Initial commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Luca Pizzarelli on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
